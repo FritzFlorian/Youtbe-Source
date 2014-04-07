284b4f5505773d495085a93e278829af86958d92
Florian <flo.fritz@t-online.de>
2014-04-07 19:57:01 +0200

    test

--- Slick2DCollisions/Game.java
+++ Slick2DCollisions/Game.java
@@ -5,50 +5,38 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.geom.*;
 
 public class Game extends BasicGame
 {
-	private Shape circle,rec,poly,line;
-	
+	private StaticLevel level;
+	private Player player;
+
 	public Game() throws SlickException {
 		super("Coding Quick Tips");
 	}
 
 
 	public void init(GameContainer gc) throws SlickException {
-		circle = new Circle (100,100,50);
-		line = new Line (100,100,150,100);
-		rec = new Rectangle( 200,200,100,50 );
+		level = new StaticLevel();
+		level.init(gc);
 		
-		float[] polyPosition = new float[] {400,400,
-											500,500,
-											300,500,
-											350,430};
-		poly = new Polygon(polyPosition);
+		player = new Player( level );
+		player.init(gc);
 	}
+	
 	
 
 	public void render(GameContainer gc, Graphics g) throws SlickException {
 		drawDebugLines( g , 50 );
 		
-		g.setColor( Color.green );
-		g.draw(line);
-		g.draw(rec);
-		g.draw(poly);
-		
-		if( circle.intersects(line) || circle.intersects(rec) || circle.intersects(poly) )
-		{
-			g.setColor(Color.red);
-		}
-		g.draw(circle);
+		level.render(gc, g);
+		player.render(gc, g);
 	}
-	
 
 
 	public void update(GameContainer gc, int delta) throws SlickException {
-		circle.setCenterX(gc.getInput().getMouseX());
-		circle.setCenterY(gc.getInput().getMouseY());
+		level.update(gc, delta);
+		player.update(gc, delta);
 	}
 	
 	
